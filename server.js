abf37a08ad673a5d376768511c9b29ac3fe1ea6e
Shivam Kumar <115875580+Badshahshvm@users.noreply.github.com>
2024-06-15 19:09:35 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,6 +7,17 @@ app.use((req, res, next) => {
   next();
 });
 app.use((req, res, next) => {
+  console.log("path", req.path, "method", req.method);
+  next();
+});
+app.use("/api", (req, res, next) => {
+  let { token } = req.query;
+  if (token === "GIVE ACCESS") {
+    res.send("data");
+  }
+  next();
+});
+app.use((req, res, next) => {
   console.log("I am second middlewares");
   next();
 });
